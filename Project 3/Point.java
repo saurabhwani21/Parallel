3749b4a71067da9454ff4021b62147760d26009e
Saurabh Wani <wani.saurabh.21@gmail.com>
2018-02-17 16:07:06 -0500

    Create Point.java

---
+++ Project 3/Point.java
@@ -0,0 +1,13 @@
+
+/**
+ * Class Point encapsulates a 3-D point.
+ *
+ * @author  Alan Kaminsky
+ * @version 10-Oct-2016
+ */
+public class Point
+	{
+	public double x;  // X coordinate
+	public double y;  // Y coordinate
+	public double z;  // Z coordinate
+	}
